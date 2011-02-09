7cc4cf23f56ec24baab62975d747104cadf5570b
unknown <Brad@.(none)>
2011-02-08 20:56:49 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Millionmonks::Application.config.secret_token = 'a5526e3d496a98b94e131e58fbf645fc94167eebdfacac3cce10d42e41d580cec6e6992e6749ccd850b2e8b97a63713906dc5af4937f711f3c9d74dd22f56944'
