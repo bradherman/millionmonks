9afd7ec61feb98af2c28f451fe1b8a82958e5eb0
Bradley Herman <bradley.t.herman@gmail.com>
2011-02-11 12:43:57 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -55,8 +55,11 @@ Millionmonks::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  get '/' => 'splash#index'
+  get '/index' => 'splash#index'
+  get '/splash/index' => 'splash#index'
   match '/' => 'splash#index'
   match '/index' => 'splash#index'
-  match 'splash/index' => 'splash#index'
+  match '/splash/index' => 'splash#index'
   root :to => "splash#index"
 end
